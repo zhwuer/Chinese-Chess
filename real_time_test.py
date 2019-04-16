5b114e240f17b7bb7ec075cdb6ee0d08029555fd
zhwuer <wjt12137@163.com>
2019-04-16 19:11:13 +0800

    change ip address

--- real_time_test.py
+++ real_time_test.py
@@ -237,8 +237,8 @@ def PiecesChangeDetection(current_step):
 
 if __name__ == '__main__':
 	# Initialize camera
-	# cap = cv2.VideoCapture("http://admin:admin@%s:8081/" % ip)
-	cap = cv2.VideoCapture('test.avi')
+	cap = cv2.VideoCapture("http://admin:admin@%s:8081/" % ip)
+	# cap = cv2.VideoCapture('test.avi')
 	if cap.isOpened():
 		for j in range(20):
 			cap.read()
