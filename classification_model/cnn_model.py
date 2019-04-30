d2b103181bbf1bc4ab871c4b8b42a9da41a2bbdf
zhwuer <wjt12137@163.com>
2019-04-30 23:22:10 +0800

    change ip address

--- classification_model/cnn_model.py
+++ classification_model/cnn_model.py
@@ -1,11 +1,13 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import matplotlib.pyplot as plt
-from keras.models import Sequential, load_model
+from keras.models import Sequential, load_model, Model
+from keras.applications import VGG19
 from keras.callbacks import ModelCheckpoint, TensorBoard
-from keras.layers import Convolution2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras.layers import Input, Convolution2D, MaxPooling2D, Activation, Dropout, Flatten, Dense, GlobalAveragePooling2D
 import os, multiprocessing, multiprocessing.pool
 NUM_CLASSES = 15
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['KMP_DUPLICATE_LIB_OK'] = "TRUE"
 root_path = '/Users/jartus/Chinese-Chess/classification_model'
 os.chdir(root_path)
 
@@ -21,46 +23,66 @@ validation_steps = 400
 
 
 def create_cnn_model():
-		model = Sequential()
-		### Conv layer 1
-		model.add(Convolution2D(
-				input_shape=(56, 56, 3),
-				filters = 32,
-				kernel_size=3,
-				strides=1,
-				padding='same',
-				data_format='channels_last',
-				activation='relu'
-		))
-		model.add(MaxPooling2D(
-				pool_size=2,
-				strides=2,
-				padding='same',
-				data_format='channels_last',
-		))
-		### Conv layer 2
-		model.add(Convolution2D(32, 3, strides=1, padding='same', data_format='channels_last', activation='relu'))
-		model.add(MaxPooling2D(2, 2, padding='same', data_format='channels_last'))
+	model = Sequential()
+	### Conv layer 1
+	model.add(Convolution2D(
+		input_shape=(56, 56, 3),
+		filters=32,
+		kernel_size=3,
+		strides=1,
+		padding='same',
+		data_format='channels_last',
+		activation='relu'
+	))
+	model.add(MaxPooling2D(
+		pool_size=2,
+		strides=2,
+		padding='same',
+		data_format='channels_last',
+	))
+	### Conv layer 2
+	model.add(Convolution2D(32, 3, strides=1, padding='same', data_format='channels_last', activation='relu'))
+	model.add(MaxPooling2D(2, 2, padding='same', data_format='channels_last'))
 
-		### Conv layer 3
-		model.add(Convolution2D(64, 3, strides=1, padding='same', data_format='channels_last', activation='relu'))
-		model.add(MaxPooling2D(2, 2, padding='same', data_format='channels_last'))
-		model.add(Dropout(0.25))
+	### Conv layer 3
+	model.add(Convolution2D(64, 3, strides=1, padding='same', data_format='channels_last', activation='relu'))
+	model.add(MaxPooling2D(2, 2, padding='same', data_format='channels_last'))
+	model.add(Dropout(0.25))
 
-		### FC
-		model.add(Flatten())
-		model.add(Dense(256, activation='relu'))
-		model.add(Dropout(0.5))
+	### FC
+	model.add(Flatten())
+	model.add(Dense(256, activation='relu'))
+	model.add(Dropout(0.5))
 
-		model.add(Dense(NUM_CLASSES, activation='softmax', name='output'))
+	model.add(Dense(NUM_CLASSES, activation='softmax', name='output'))
 
-		model.compile(optimizer='Adam',
-					  loss='categorical_crossentropy',
-					   metrics=['accuracy'])
-		return model
+	model.compile(optimizer='Adam',
+				  loss='categorical_crossentropy',
+				  metrics=['accuracy'])
+	model.load_weights(loaded_model_path, by_name=True)
+	return model
 
-model = create_cnn_model()
-model.load_weights(loaded_model_path, by_name=True)
+def create_vgg_model():
+	base_model = VGG19(weights='imagenet', include_top=False, input_shape=(56, 56, 3))
+	x = base_model.output
+	x = GlobalAveragePooling2D()(x)
+	x = Dense(1024, activation='relu')(x)
+	x = Dense(512, activation='relu')(x)
+	x = Dense(256, activation='relu')(x)
+	x = Dropout(0.5)(x)
+	x = Dense(NUM_CLASSES, activation='softmax', name='output')(x)
+	model = Model(inputs=base_model.input, outputs=x)
+	for layer in model.layers[:20]:
+		layer.trainable = False
+	for layer in model.layers[20:]:
+		layer.trainable = True
+	model.compile(optimizer='Adam',
+				  loss='categorical_crossentropy',
+				  metrics=['accuracy'])
+	return model
+
+#model = create_cnn_model()
+model = create_vgg_model()
 model.summary()
 #pool = multiprocessing.Pool(processes=8)
 
